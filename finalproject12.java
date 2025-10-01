2f25a17ca464dff11f3568b5a5bf11db84fd2ccb
Adamavito12 <email.lgadam20@gmail.com>
2025-10-01 14:04:09 +0700

    user2

--- finalproject12.java
+++ finalproject12.java
@@ -1,6 +1,7 @@
 public class finalproject12{
    public static void main(String[] args) {
      System.out.println("hi,ini baris kode pertaama");
+     System.out.println("hi,ini baris kode kedua");
    }
 
 }
